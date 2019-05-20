b0e16d4ef8bf53068534bbcc8c082c3ca68630e0
Will <will889889@gmail.com>
2019-05-21 01:15:02 +0800

    Start developing Caculator

---
+++ Optimization/Optimization/Caculator.cpp
@@ -0,0 +1,19 @@
+#include"Caculator.h"
+using namespace std;
+
+Caculator::Caculator()
+{
+	currentFunc = NULL;
+}
+
+void Caculator::LoadFunction(string input)
+{
+
+}
+
+double Caculator::Caculate(map<char, double>)
+{
+	double output = 0.0f;
+
+	return output;
+}
