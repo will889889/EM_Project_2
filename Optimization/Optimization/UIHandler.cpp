65f5c98b0e7d0d755626910c4ab9d9c9579f8882
ian287913 <45512766+ian287913@users.noreply.github.com>
2019-05-21 16:47:28 +0800

    fix golden experience

--- Optimization/Optimization/UIHandler.cpp
+++ Optimization/Optimization/UIHandler.cpp
@@ -21,6 +21,7 @@ std::vector<std::string> UIHandler::DoMath()
 
 	sstream << m_Input;
 	sstream >> equationIndex;
+	/// variable amount needed
 	sstream >> initPoint;
 
 	///	Equation analysis >> get variable amount
@@ -77,46 +78,100 @@ std::vector<std::string> UIHandler::DoMath()
 std::vector<std::string> Answer;
 
 #pragma region GoldenSection
-double phi = (1 + std::sqrt(5)) / 2;
-double resphi = 2 - phi;
+//	The function
 double f(double x)
 {
-	return (x*(x - 1.0));
+	return (std::sin(3.0*x) + std::cos(x));
+	//return (x*(x - 1.0));
 	//return ((21.0 / 4.0) + (x - (1.0 / 4.0)) * (x - (1.0 / 4.0)));
 }
+///	boost
+double phi = (1 + std::sqrt(5)) / 2;	// 1.6xx
+double resphi = 2 - phi;	// 0.3xx
+double fof_x, fof_b, x;
+bool Bias_Left;
+///	boost
+int Iter_Threshold = 1000;
+int Iter_Count;
+//	here, tau means the threshold of delta( f(b1), f(b2) )
 double goldenSectionSearch(double a, double b, double c, double tau)
 {
-	Answer.push_back(std::to_string(b));
-	double x;
-	if (c - b > b - a)
-		x = b + resphi * (c - b);
-	else
-		x = b - resphi * (b - a);
+	//	find x (b2)
+	Bias_Left = (std::abs(c - b) > std::abs(b - a));	//	b is on left
 
-	if (std::abs(c - a) < tau * (std::abs(b) + std::abs(x)))
-		return (c + a) / 2;
-	if (f(x) < f(b))
+	if (Bias_Left)
+		x = c - resphi * (c - a);
+	else
+		x = a + resphi * (c - a);
+
+	//Answer.push_back(" x: " + std::to_string(b) + " , b: " + std::to_string(x) + " , ca: " + std::to_string(std::abs(a - c)));
+
+	//	calculate (get y)
+	fof_x = f(x);
+	fof_b = f(b);
+
+	//	threshold - delta( f(b1), f(b2) )
+	if ((std::abs(fof_x - fof_b) < tau) || (Iter_Count > Iter_Threshold))
+		return (x + b) / 2;
+	////	threshold - delta( b1, b2 )
+	//if (std::abs(a - c) < tau)
+	//	return (x + b) / 2;
+
+	//	compare
+	if (fof_x < fof_b)
 	{
-		if (c - b > b - a)
+		if (Bias_Left)	//	if b1 is on left
 			return goldenSectionSearch(b, x, c, tau);
 		else
 			return goldenSectionSearch(a, x, b, tau);
 	}
 	else
 	{
-		if (c - b > b - a)
+		if (Bias_Left)
 			return goldenSectionSearch(a, b, x, tau);
 		else
 			return goldenSectionSearch(x, b, c, tau);
 	}
 }
+//double goldenSectionSearch(double a, double b, double c, double tau)
+//{
+//	//Answer.push_back(std::to_string(b));
+//	//	find x (b2)
+//	double x;
+//	if (c - b > b - a)
+//		x = b + resphi * (c - b);
+//	else
+//		x = b - resphi * (b - a);
+//
+//	//	threshold
+//	if (std::abs(c - a) < tau * (std::abs(b) + std::abs(x)))
+//		return (c + a) / 2;
+//
+//	//	compare
+//	if (f(x) < f(b))
+//	{
+//		if (c - b > b - a)
+//			return goldenSectionSearch(b, x, c, tau);
+//		else
+//			return goldenSectionSearch(a, x, b, tau);
+//	}
+//	else
+//	{
+//		if (c - b > b - a)
+//			return goldenSectionSearch(a, b, x, tau);
+//		else
+//			return goldenSectionSearch(x, b, c, tau);
+//	}
+//}
 #pragma endregion
 
 std::vector<std::string> UIHandler::TEST()
 {
 	Answer.clear();
+	Iter_Count = 0;
 
-	Answer.push_back(std::to_string(goldenSectionSearch(0.0, (2.0*resphi), 2.0, 0.1)));
-	
+	//Answer.push_back(std::to_string(goldenSectionSearch(-0.5, (5.0*resphi - 0.5), 4.5, 0.00000001)) + " <- ans");
+	Answer.push_back(std::to_string(goldenSectionSearch(0.3, (2.7*resphi + 0.3), 3.0, 0.00000001)) + " <- ans");
+
 	return Answer;
 }
