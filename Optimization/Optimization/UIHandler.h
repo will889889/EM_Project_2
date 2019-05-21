5ab4d0382ffc77cfc4d11d7c865d8ccc50308c94
ian287913 <45512766+ian287913@users.noreply.github.com>
2019-05-21 21:22:17 +0800

    Powells

--- Optimization/Optimization/UIHandler.h
+++ Optimization/Optimization/UIHandler.h
@@ -21,9 +21,17 @@ public:
 	UIHandler();
 
 	std::string m_Input;
+	std::vector<std::string> Answer;
+	//	for calcu
 	Method m_Method;
 	std::vector<std::string> equations;
+	int equationIndex;
+	int varsCount;
+	std::vector<double> initPoint;
+	std::vector< std::pair<double, double> > intervals;
 
 	std::vector<std::string> DoMath();	//Method _method, double _initPoint, double _leftBound, double _rightBound
+
+	std::vector<std::string> Powell_method();
 	std::vector<std::string> TEST();
 };
