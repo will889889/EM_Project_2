03009ac570fa8064f0068b8bbb945a66234e6883
Will <will889889@gmail.com>
2019-05-23 01:05:53 +0800

    Caculator now can differential non-sin-cos functions!

--- Optimization/Optimization/Caculator.h
+++ Optimization/Optimization/Caculator.h
@@ -10,11 +10,17 @@ class Caculator
 public:
 	Caculator();
 	
+	Term* myCurrentFunc();
 	std::vector<char> LoadFunction(std::string);
 	double Caculate(std::map<char, double>);
+	double Caculate(Term*, std::map<char, double>);
+	Term* PartialDiff(Term* , char);
+	double PartialDerivative(Term*, char, std::map<char, double>);
 
 private:
 	Term* currentFunc;
 	Term* LoadTerm(std::string input);
 	std::vector<char> FindVarInTerm(Term*, std::vector<char>);
+	void Destoryer(Term*);
+	void Destoryer(Variable*);
 };
